b04809f9291be80c9691a70439699b01ce3a669e
anovesp01 <anovesp01@educastillalamancha.es>
2024-04-16 11:11:28 +0200

    Version inicial

---
+++ src/main/java/es/mdc/FUtil.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.mdc;
+import java.io.File;
+
+/**
+ *
+ * @author Usuario
+ */
+public class FUtil {
+    
+    public static boolean existe(String filename){
+        File file = new File(filename);
+        if(file.exists()){
+            return true;
+        }
+        if(filename.isBlank()){
+            throw new IllegalArgumentException("La cadena no puede estar en blanco.");
+        }
+        return false;
+    }
+}
---
+++ src/main/java/es/mdc/FUtilApp.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package es.mdc;
+
+import static es.mdc.FUtil.existe;
+
+/**
+ *
+ * @author Usuario
+ */
+public class FUtilApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String archivo1 = "Documentos";
+    String archivo2 = "Imágenes";
+    String archivo3 = "Vídeos";
+    boolean respuesta1 = existe(archivo1);
+    boolean respuesta2 = existe(archivo2);
+    boolean respuesta3 = existe(archivo3);
+    
+        System.out.printf("Documentos: %b%n Imágenes: %b%n Vídeos: %b%n", respuesta1, respuesta2, respuesta3);
+    }
+}
